28364df40cd771e4fe1c51a8271eac020df9526a
Le.Sigwal <61907278+le-sigwal@users.noreply.github.com>
2020-03-14 09:17:15 +0100

    Added avifinfo/filetypebox

--- wavif.cpp
+++ wavif.cpp
@@ -1,23 +1,24 @@
 //-------------------------------------------------------------------------
 /* File: wavif.cpp
  * Description: "Work Around aVIF" Project (AV1 Image File Format)
- *                Container: 
+ *                Container:
  *                  ISO-BMFF (ISO Base Media File Format)
  *                    https://en.wikipedia.org/wiki/ISO/..
  *                            ..IEC_base_media_file_format
  *                  HEIF (High Efficiency Image File Format)
  *                    https://nokiatech.github.io/heif/
- *                Codec    : 
+ *                Codec    :
  *                  AV1
  *                    https://aomediacodec.github.io/av1-avif/
  * Authors: Bagad Sigwal
  * Copyright: 2020 Sigwal.info/github
- * Last modification: 10/03/2020
+ * Last modification: 14/03/2020
  * Version: 20.03
  *
  * Change records:
  * SWL - 09/03/2020 - creation, added main()
- * SWL - 10/03/2020 - added avifinfo()                                   */
+ * SWL - 10/03/2020 - added avifinfo()
+ * SWL - 14/03/2020 - added avifinfo/filetypebox                         */
 //-------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------
@@ -29,6 +30,11 @@
 //[>PROJECT DEFINITIONS]
 //[>LOCAL DEFINITIONS]
 #define WAVIF_VERSION "20.03"
+//
+typedef union uint32_u {
+	uint32_t ui;
+	char     cr[4];
+} uint32_o; // octet
 //-------------------------------------------------------------------------
 //[<DEFINITIONS]
 //-------------------------------------------------------------------------
@@ -38,14 +44,47 @@
 //-------------------------------------------------------------------------
 int avifinfo(const char *filepath){
 
-	std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<std::endl;
+	std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<std::endl;
 	std::cout<<"[>AVIF FILE INFORMATIONS]"<<std::endl;
-	std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<std::endl;
+	std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<std::endl;
 	std::cout<<"filepath: '"<<filepath<<"'"<<std::endl;
-	//[af]
-	std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
+
+	FILE *avifile = fopen(filepath, "rt");
+	if (avifile == NULL){
+		return -1;
+	}
+
+	std::cout<<"  - File Type Box"<<std::endl;
+	uint32_o bhsize; /*Box header size*/
+	char ac=fgetc(avifile), bc=fgetc(avifile), cc=fgetc(avifile), dc=fgetc(avifile);
+	bhsize.cr[0]=dc; bhsize.cr[1]=cc; bhsize.cr[2]=bc; bhsize.cr[3]=ac;
+	std::cout<<"    - Box header size: "<<bhsize.ui<<std::endl;
+
+	ac=fgetc(avifile); bc=fgetc(avifile); cc=fgetc(avifile); dc=fgetc(avifile);
+	std::cout<<"    - Box header type: '"<<ac<<bc<<cc<<dc<<"'"<<std::endl;
+
+	ac=fgetc(avifile); bc=fgetc(avifile); cc=fgetc(avifile); dc=fgetc(avifile);
+	std::cout<<"    - Brand: '"<<ac<<bc<<cc<<dc<<"'"<<std::endl;
+
+	uint32_o minvers; /*Minor version*/
+	ac=fgetc(avifile); bc=fgetc(avifile); cc=fgetc(avifile); dc=fgetc(avifile);
+	minvers.cr[0]=dc; minvers.cr[1]=cc; minvers.cr[2]=bc; minvers.cr[3]=ac;
+	std::cout<<"    - Minor version: "<<minvers.ui<<std::endl;
+
+	std::cout<<"    - Compatible Brands"<<std::endl;
+	int cbn = (bhsize.ui-4*4)/4, cbi;
+	for(cbi=0;cbi<cbn;cbi++){
+		ac=fgetc(avifile); bc=fgetc(avifile); cc=fgetc(avifile); dc=fgetc(avifile);
+		std::cout<<"      - '"<<ac<<bc<<cc<<dc<<"'"<<std::endl;
+	}
+
+
+	fclose(avifile);
+
+
+	std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
 	std::cout<<"[<AVIF FILE INFORMATIONS]"<<std::endl;
-	std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
+	std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
 
 	return 0;
 }
